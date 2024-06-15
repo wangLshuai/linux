a3fd5855b5b7521a913d5827ed92a25d702a7a2a
wangshuai <wang.lin.shuai@qq.com>
2024-06-15 12:44:44 +0800

    register myusb_hid driver in usb bus define probe and disconnect function in usb_driver

--- myproject/myusb_hid/myusb_hid.c
+++ myproject/myusb_hid/myusb_hid.c
@@ -1,14 +1,45 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/usb.h>
+#include <linux/hid.h>
 
+static int myusb_hid_probe(struct usb_interface *intf,
+			   const struct usb_device_id *id)
+{
+	mylog("match hid interface\n");
+	return 0;
+}
+
+static void myusb_hid_disconnect(struct usb_interface *intf)
+{
+	mylog("called when remove module\n");
+}
+
+// match interface class is HID
+static struct usb_device_id myusb_hid_ids[] = {
+	{ .match_flags = USB_DEVICE_ID_MATCH_INT_CLASS,
+	  .bInterfaceClass = USB_INTERFACE_CLASS_HID },
+	{}
+};
+// interface driver
+static struct usb_driver myusb_hid_driver = {
+	.name = "myusb_hid",
+	.probe = myusb_hid_probe,
+	.disconnect = myusb_hid_disconnect,
+	.id_table = myusb_hid_ids,
+};
 static int __init myusb_hid_init(void)
 {
 	printk("myusb_hid init \n");
+	printk("KBUILD_MODNAME:%s,THIS_MODULE:%llu\n", KBUILD_MODNAME,
+	       (uint64_t)THIS_MODULE);
+	usb_register(&myusb_hid_driver);
 	return 0;
 }
 
 static void __exit myusb_hid_exit(void)
 {
+	usb_deregister(&myusb_hid_driver);
 	printk("myusb_hid exit \n");
 }
 
